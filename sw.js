e74286523ef31b8b2af5cd06ca78d30d0a01ac8b
j27pwyt2tn-wq <j27pwyt2tn@privaterelay.appleid.com>
2026-09-16 12:23:07 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="health-tracker-v4";
+const CACHE="health-tracker-v5";
 self.addEventListener("install",e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./sw.js"])));
